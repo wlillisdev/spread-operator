81acaaf01ef738338f44fdfc72a21cd272f6e04a
wlillisdev <williamlillis100@gmail.com>
2022-04-03 07:58:57 +0000

    intal commit

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,9 +4,25 @@
  */
 
 // No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4);
+console.log("second array:",arr1);
 
 // Copying an array
+let arr3 =[4,5,6];
+let arr4 =[...arr3];
+arr4.push(7);
+console.log("third array:",arr3);
+console.log("forth array:",arr4);
 
 // Copying an object
 
+let obj1 = {a: 1, b: 2, c: 3};
+let obj2 = {...obj1, d: 4};
+console.log("first object:",obj1);
+console.log("first object:",obj2);
+
 // Copying only part of an array/object
+let arr5 = [...arr1,{...obj1},...arr3, "x","y","z"]
+console.log("Array 5:",arr5);
